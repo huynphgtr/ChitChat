3830f0ed8af96fb03cafd293d7a96e84379b610f
Trần Phương Huyền <tranphuonghuyen2005@gmail.com>
2025-06-23 22:22:02 +0700

    create routes and components for UI

--- ChitChatApp.Blazor/tailwind.config.js
+++ ChitChatApp.Blazor/tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./Components/**/*.{razor,html,cshtml}",
-    "./Pages/**/*.{razor,html,cshtml}",
-    "./Shared/**/*.{razor,html,cshtml}",
-    "./Views/**/*.{razor,html,cshtml}",
-    "./Areas/**/*.{razor,html,cshtml}",
-    "./wwwroot/**/*.html",
-    "./**/*.razor"
-  ],
+module.exports = {
+    content: [
+        './**/*.razor',
+        './**/*.cshtml',
+        './**/*.html',
+        './wwwroot/index.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
+
 
