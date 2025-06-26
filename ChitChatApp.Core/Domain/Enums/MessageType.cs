a8c21cf4951b71ff8f73f7834788c48a98861c8f
PTC1102 <pt.cuong.1102@gmail.com>
2025-06-26 14:12:02 +0700

    create enums

---
+++ ChitChatApp.Core/Domain/Enums/MessageType.cs
@@ -0,0 +1,29 @@
+﻿namespace ChitChatApp.Core.Domain.Enums;
+
+public enum MessageType
+{
+    Text,
+    Image,
+    File,
+    System
+}
+
+public static class MessageTypeExtensions
+{
+    public static string ToDbValue(this MessageType type)
+    {
+        return type.ToString().ToLowerInvariant();
+    }
+
+    public static MessageType FromDbValue(string value)
+    {
+        return value.ToLowerInvariant() switch
+        {
+            "text" => MessageType.Text,
+            "image" => MessageType.Image,
+            "file" => MessageType.File,
+            "system" => MessageType.System,
+            _ => MessageType.Text
+        };
+    }
+}
---
+++ ChitChatApp.Core/Domain/Enums/UserRole.cs
@@ -0,0 +1,27 @@
+﻿namespace ChitChatApp.Core.Domain.Enums;
+
+public enum UserRole
+{
+    Admin,
+    Member,
+    None
+}
+
+public static class UserRoleExtensions
+{
+    public static string ToDbValue(this UserRole role)
+    {
+        return role.ToString().ToLowerInvariant();
+    }
+
+    public static UserRole FromDbValue(string value)
+    {
+        return value.ToLowerInvariant() switch
+        {
+            "admin" => UserRole.Admin,
+            "member" => UserRole.Member,
+            "none" => UserRole.None,
+            _ => UserRole.None
+        };
+    }
+}
